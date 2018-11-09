72a8c77cc589b28a559de8a0a2d929a820922be6
Chad Morrow <35667376+ToxicCM72@users.noreply.github.com>
2018-11-09 15:42:46 +0000

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        zombie zombie = new zombie();
+        player player = new player();
+
+        player.TakeDamage(zombie.Attack());
+        player.TakeDamage(zombie.AcidPuke());
+
+        player.Die();
+
+        zombie[] zombies = new zombie[100];
+
+        for (int x; x < zombies.Length; x++) {
+            zombies[x] = new zombie();
+            print("Create Zombie #: " + x);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : Humanoid {
+
+    int spinAttackDamage = 10;
+
+    public override int Attack() {
+        return attackDamage + spinAttackDamage;
+    }
+}
