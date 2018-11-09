5a2e68d310ad5ab90c2d27f22e57993cd7e75bd6
Chad Morrow <35667376+ToxicCM72@users.noreply.github.com>
2018-11-09 11:48:01 +0000

    Add files via upload

---
+++ ListTimeBaby.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListTimeBaby : MonoBehaviour {
+
+    public GameObject cubePrefab;
+
+    Color[] colors = { Color.black, Color.red, Color.blue, Color.yellow };
+
+    GameObject[] cubes = new GameObject[5];
+
+    float spacer = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+
+        for (int i = 0; i < cubes.Length; i++) {
+
+            GameObject cube = Instantiate(cubePrefab);
+            cubes[i] = cube;
+            cube.transform.position = new Vector3(i + (spacer * i), cube.transform.position.y);
+            cube.GetComponent<Renderer>().material.color = colors[i];
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ variables.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        string name = "Thanos";
+        string equipedWeapon = "Infinity Gauntlet";
+        string favoriteFurniture = "Throne";
+
+        string favoritePlanet;
+        favoritePlanet = "Earth";
+
+        Debug.Log(name);
+        Debug.Log(equipedWeapon);
+        Debug.Log(favoriteFurniture);
+
+        Debug.Log(equipedWeapon.ToUpper());
+
+        double hp = 100.0;
+        double shieldPower = 76.5;
+        double laserDamage = 30;
+        double acutalDamagePercent = .05;
+
+        double actualDamage = (laserDamage * acutalDamagePercent);
+
+        hp -= actualDamage;
+        shieldPower -= (laserDamage - actualDamage);
+
+        Debug.Log("HP: " + hp);
+        Debug.Log("Shield Power: " + shieldPower);
+
+        int slices = 10 / 5;
+
+        print(slices);
+
+        int newDamage = 10 / 3;
+
+        print(newDamage);
+
+        int newDamageRemainder = 10 % 3;
+
+        print("10 divided by 3 equals " + newDamage + " with a remainder of " + newDamageRemainder);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
